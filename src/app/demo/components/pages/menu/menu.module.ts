cbc742b35d523322b17f7532381bb54dbc0f28eb
Anh Quan <quandang123654@gmail.com>
2023-12-10 18:25:35 +0700

    Update Menu

--- src/app/demo/components/pages/menu/menu.module.ts
+++ src/app/demo/components/pages/menu/menu.module.ts
@@ -10,6 +10,10 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { Toast, ToastModule } from 'primeng/toast';
+import { InputNumber, InputNumberModule } from 'primeng/inputnumber';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @NgModule({
     declarations: [MenuComponent],
@@ -23,6 +27,10 @@ import { ToolbarModule } from 'primeng/toolbar';
         SidebarModule,
         ButtonModule,
         ToolbarModule,
+        DropdownModule,
+        ToastModule,
+        InputNumberModule,
+        OverlayPanelModule,
     ],
 })
 export class MenuModule {}
